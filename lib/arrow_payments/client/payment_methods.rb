7cc758d9f0c31deba56594c51138382800b7e0da
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-12-20 15:23:13 -0600

    Add payment_method_start Step 1 method

--- lib/arrow_payments/client/payment_methods.rb
+++ lib/arrow_payments/client/payment_methods.rb
@@ -1,5 +1,26 @@
 module ArrowPayments
   module PaymentMethods
-    # TODO: Implementation
+    # Start a new payment method
+    # @param [Integer] customer ID
+    # @param [Address] billing address instance
+    # #param [String] return url
+    def payment_method_start(customer_id, billing_address, return_url=nil)
+      if billing_address.kind_of?(Hash)
+        billing_address = ArrowPayments::Address.new(billing_address)
+      end
+
+      params = {
+        'CustomerId'     => customer_id,
+        'BillingAddress' => billing_address.to_source_hash
+      }
+
+      # If return url is blank means that its not browser-less payment method
+      # creation. Reponse should include token ID for the Step 3.
+      if return_url
+        params['ReturnUrl'] = return_url
+      end
+
+      post("/paymentmethod/start", params)
+    end
   end
 end
