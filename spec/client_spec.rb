2396769810ceface97be3c45c8f682f06a716020
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-01-08 20:48:40 -0600

    Make sure that merchant ID is required

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,19 +6,25 @@ describe ArrowPayments::Client do
       expect { ArrowPayments::Client.new }.to raise_error "API key required"
     end
 
+    it 'requires a merchant id' do
+      expect { ArrowPayments::Client.new(:api_key => 'foobar') }.
+        to raise_error "Merchant ID required"
+    end
+
     it 'validates api mode' do
-      expect { ArrowPayments::Client.new(:api_key => 'foobar', :mode => 'foobar') }.to raise_error "Invalid mode: foobar"
+      expect { ArrowPayments::Client.new(:api_key => 'foobar', :merchant_id => 'foobar', :mode => 'foobar') }.
+        to raise_error "Invalid mode: foobar"
     end
 
     it 'sets production mode by default' do
-      client = ArrowPayments::Client.new(:api_key => 'foobar')
+      client = ArrowPayments::Client.new(:api_key => 'foobar', :merchant_id => 'foobar')
       client.mode.should eq('production')
     end
   end
 
   describe '#sanbox?' do
     it 'returns true if sandbox mode' do
-      client = ArrowPayments::Client.new(:api_key => 'foobar', :mode => 'sandbox')
+      client = ArrowPayments::Client.new(:api_key => 'foobar', :merchant_id => 'foobar', :mode => 'sandbox')
 
       client.sandbox?.should be_true
       client.production?.should_not be_true
@@ -27,7 +33,7 @@ describe ArrowPayments::Client do
 
   describe '#production?' do
     it 'returns true if production mode' do
-      client = ArrowPayments::Client.new(:api_key => 'foobar', :mode => 'production')
+      client = ArrowPayments::Client.new(:api_key => 'foobar', :merchant_id => 'foobar', :mode => 'production')
 
       client.sandbox?.should be_false
       client.production?.should be_true
