ea7cabe1be6b4fd06ed9ddfc6e0142437dfa1175
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-01-08 20:39:35 -0600

    Add functionality and spec to recurring billing

--- lib/arrow_payments/recurring_billing.rb
+++ lib/arrow_payments/recurring_billing.rb
@@ -1,5 +1,17 @@
+# From documentation:
+# * Frequency -> W, M, Q, Y (weekly, monthly, quarterly, yearly)
+# * TransactionDay -> Weekly: [1-7], Monthly: [1-28], Quarterly:[1,2,3], Yearly: [1-12]
+
 module ArrowPayments
   class RecurringBilling < Entity
+    FREQUENCIES = %w(W M Q Y)
+    FREQUENCY_NAMES = {
+      'W' => 'Weekly',
+      'M' => 'Monthly', 
+      'Q' => 'Quarterly',
+      'Y' => 'Yearly'
+    }
+
     property :id,                :from => 'ID'
     property :payment_method_id, :from => 'PaymentMethodId'
     property :frequency,         :from => 'Frequency'
@@ -8,5 +20,11 @@ module ArrowPayments
     property :description,       :from => 'Description'
     property :transaction_day,   :from => 'TransactionDay'
     property :date_created,      :from => 'DateCreated'
+
+    # Get billing frequency name
+    # @return [String]
+    def frequency_name
+      FREQUENCY_NAMES[frequency]
+    end
   end
 end
